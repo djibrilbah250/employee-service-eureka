62abcd32050dee47a663795928f983fc9eb906eb
djibril.bah <djibril.bah@it-centrex.com>
2026-09-14 18:16:11 +0000

    config

--- src/main/java/com/example/employee_service_eureka/feignclient/AddressClient.java
+++ src/main/java/com/example/employee_service_eureka/feignclient/AddressClient.java
@@ -6,10 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "address-service", url = "http://localhost:8081", path = "/address-service")
+@FeignClient(name = "address-service", url = "http://localhost:8002", path = "/address-service")
 public interface AddressClient {
 
     @GetMapping("/address/{id}")
-    public ResponseEntity<AddressResponse> getAddressByEmployeeId(@PathVariable("id") int id);
+    ResponseEntity<AddressResponse> getAddressByEmployeeId(@PathVariable("id") int id);
 
 }
