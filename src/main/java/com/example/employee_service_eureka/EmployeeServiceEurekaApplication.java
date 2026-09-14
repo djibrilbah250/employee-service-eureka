62bfe6126465d8c863dfd461b9252ae81f3f70a1
djibril.bah <djibril.bah@it-centrex.com>
2026-09-14 16:06:44 +0000

    first commit

---
+++ src/main/java/com/example/employee_service_eureka/EmployeeServiceEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_service_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServiceEurekaApplication.class, args);
+	}
+
+}
